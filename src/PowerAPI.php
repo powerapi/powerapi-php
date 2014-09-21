9c52b2ef2bee627e654daf921012eaa18480352d
Henri Watson <henri@henriwatson.com>
2014-09-21 13:31:45 -0400

    clean up phpDoc

--- src/PowerAPI.php
+++ src/PowerAPI.php
@@ -1,51 +1,27 @@
 <?php
 
-/**
- * Copyright (c) 2014 Henri Watson
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * @author         Henri Watson
- * @package        PowerAPI
- * @version        3.0
- * @license        http://opensource.org/licenses/MIT    The MIT License
- */
-
-
+require_once 'PowerAPI/BaseObject.php';
 require_once 'PowerAPI/Exception.php';
+require_once 'PowerAPI/Parser.php';
 
-require_once 'PowerAPI/Course.php';
+require_once 'PowerAPI/Assignment.php';
+require_once 'PowerAPI/Section.php';
 require_once 'PowerAPI/Student.php';
 
 /** Handles the initial token fetch and login */
 class PowerAPI
 {
-    private $ua = 'PowerAPI-php/3.0 (https://github.com/powerapi/powerapi-php)';
-
     /**
      * Attempt to authenticate against the server
-     * @param string username
-     * @param string password
-     * @return User
-    */
+     * @param string $url URL for the PowerSchool server to authenticate against
+     * @param string $username student's username
+     * @param string $password student's password
+     * @param boolean $fetch_transcript fetch transcript after successful login?
+     * @return PowerAPI\User
+     */
     static public function authenticate($url, $username, $password, $fetch_transcript = true)
     {
+        // Ensure the URL ends with a /
         if (substr($url, -1) !== "/") {
             $url = $url."/";
         } else {
@@ -61,8 +37,16 @@ class PowerAPI
             'use' => SOAP_LITERAL
         ));
 
-        $login = $client->__call('login', Array('username' => $username, 'password' => $password, 'userType' => 2));
+        $login = $client->__call(
+            'login',
+            Array(
+                'username' => $username,
+                'password' => $password,
+                'userType' => 2
+            )
+        );
 
+        // userSessionVO is unset if something went wrong during auth.
         if ($login->userSessionVO === null) {
             throw(new PowerAPI\Exception($login->messageVOs->description));
         }
--- src/PowerAPI/BaseObject.php
+++ src/PowerAPI/BaseObject.php
@@ -1,44 +1,20 @@
 <?php
 
-/**
- * Copyright (c) 2014 Henri Watson
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included
- * in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
- * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
- * DEALINGS IN THE SOFTWARE.
- *
- * @author         Henri Watson
- * @version        3.0
- * @license        http://opensource.org/licenses/MIT    The MIT License
- */
-
 namespace PowerAPI;
 
 /** Handles the basic overrides that data holding classes make use of. */
 class BaseObject
 {
-    /** Details store for the object */
+    /** Details store for the object
+     * @var array
+     */
     protected $details;
 
     /**
      * Populates the internal details store
      * @param array $details the details to be stored
      * @return void
-    */
+     */
     public function __construct($details)
     {
         $this->details = $details;
@@ -48,7 +24,7 @@ class BaseObject
      * Returns a value from the details store
      * @param string $name key for the value to be returned
      * @return mixed value
-    */
+     */
     public function __get($name)
     {
         return $this->details[$name];
@@ -58,7 +34,7 @@ class BaseObject
      * Checks if a key exists in the details store
      * @param string $name key to be checked
      * @return boolean
-    */
+     */
     public function __isset($name)
     {
         return isset($this->details[$name]);
